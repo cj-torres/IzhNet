6301731fa4c36f1dfe02cfb0fcefd6e6771bc902
cj-torres <52362149+cj-torres@users.noreply.github.com>
2024-01-23 15:03:56 -0800

    Starting abstract frameworks

--- src/SNN_abs.jl
+++ src/SNN_abs.jl
@@ -13,3 +13,42 @@ mutable struct IzhSuperNetwork <: IzhNetwork
     nodes::Vector{IzhNetwork}
     connections::Dict{Tuple{Int, Int}, BidirectionalConnection}
 end
+
+
+struct IzhParameters{T <: AbstractArray{<:AbstractFloat}}
+    # time scale recovery parameter
+    a::T
+    # sensitivty to sub-threshold membrane fluctuations (greater values couple v and u)
+    b::T
+    # post-spike reset value of membrane potential v
+    c::T
+    # post-spike reset of recovery variable u
+    d::T
+
+    function IzhParameters(a::T, b::T, c::T, d::T)
+        @assert length(a) == length(b) == length(c) == length(d)
+    end
+end
+
+
+# Functions for returning parameters based on presets
+
+function StandardSpikingParams()
+    
+end
+
+function SimpleExcitatoryParams()
+    
+end
+
+function SimpleInhibitoryParams()
+    
+end
+
+function StandardChatteringParams()
+    
+end
+
+function StandardResonantParams()
+    
+end
